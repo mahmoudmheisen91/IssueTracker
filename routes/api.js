2b973dda780501a1ea66764a82317d94e15c86be
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-04-12 08:47:32 +0300

    implement put request

--- routes/api.js
+++ routes/api.js
@@ -43,7 +43,46 @@ module.exports = function (app, db) {
     })
 
     .put(function (req, res) {
-      var project = req.params.project;
+      let project = req.params.project;
+      let _id = req.body._id;
+      let issue = {
+        issue_title: req.body.issue_title || "",
+        issue_text: req.body.issue_text || "",
+        created_by: req.body.created_by || "",
+        assigned_to: req.body.assigned_to || "",
+        status_text: req.body.status_text || "",
+        open: req.body.open || "",
+      };
+
+      for (let item in issue) {
+        if (issue[item] === "") {
+          delete issue[item];
+        }
+      }
+
+      let error = "no updated field sent";
+      if (!Object.keys(issue).length) {
+        res.send(error);
+        res.end();
+      } else {
+        issue.updated_on = new Date();
+        let new_issue = { _id: new ObjectId(_id) };
+        db.collection(project).findAndModify(
+          new_issue,
+          [["_id", 1]],
+          { $set: issue },
+          { new: true },
+          (err, doc) => {
+            if (err) {
+              res.send("could not update " + _id);
+              res.end();
+            } else {
+              res.send("successfully updated");
+              res.end();
+            }
+          }
+        );
+      }
     })
 
     .delete(function (req, res) {
